abab837e0e1b706645d853fa667db7b021935bfe
baijiku <55678037+baijiku@users.noreply.github.com>
2019-09-23 15:00:17 +0900

    Add files via upload

--- discord_bot.py
+++ discord_bot.py
@@ -1,13 +1,17 @@
 import discord
 from datetime import datetime, timedelta
 
+TOKEN = "NjI1NTA0NjgyMzkwODQ3NDk4.XYhbGA.4Au1O_jje4dsZ77KK31DQ84QwQM"
+SEVER_ID = 625297697850785798
+TEXT_CHANNEL = 625569697441644545
+
 client = discord.Client()
 
 @client.event
 async def on_voice_state_update(member, before, after): 
-    if member.guild.id == 625297697850785798 and (before.channel != after.channel):
+    if member.guild.id == SEVER_ID and (before.channel != after.channel):
         now = datetime.utcnow() + timedelta(hours=9)
-        alert_channel = client.get_channel(625297697850785800)
+        alert_channel = client.get_channel(TEXT_CHANNEL)
         if before.channel is None: 
             msg = f'{now:%m/%d-%H:%M} に {member.name} が {after.channel.name} に参加しました。'
             await alert_channel.send(msg)
@@ -15,4 +19,4 @@ async def on_voice_state_update(member, before, after):
             msg = f'{now:%m/%d-%H:%M} に {member.name} が {before.channel.name} から退出しました。'
             await alert_channel.send(msg)
 
-client.run("NjI1NTA0NjgyMzkwODQ3NDk4.XYg5fQ.RT35Mu4yYKsprt7tMkikl_subHE")
+client.run(TOKEN)
